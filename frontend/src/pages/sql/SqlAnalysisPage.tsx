e9313555304f10194c87bdaea3662baf09d644d8
박태정 <tail1@taejungui-MacBookPro.local>
2026-07-05 16:29:14 +0900

    Set SQL context page size to ten

--- frontend/src/pages/sql/SqlAnalysisPage.tsx
+++ frontend/src/pages/sql/SqlAnalysisPage.tsx
@@ -51,7 +51,7 @@ type DerivedDatasetDraft = {
 };
 
 const PREVIEW_ROW_LIMIT = 100;
-const SQL_CONTEXT_PAGE_SIZE = 8;
+const SQL_CONTEXT_PAGE_SIZE = 10;
 const { Parser: SqlParser } = postgresqlParser;
 const sqlParser = new SqlParser();
 
